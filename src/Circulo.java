d9543042442ac1e22b96c3c1e541888c86382676
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-09-24 13:52:00 +0200

    progreso del circulo

---
+++ src/Circulo.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+
+public class Circulo {
+
+ 
+
+	public double area (double radio)   {
+
+		double result = 0;
+		result = 3.1416 * Math.pow(radio,2);
+        	return result;
+	}
+
+	
+}
---
+++ src/OperacionesCirculo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class OperacionesCirculo {
+
+	public static void main (String args[]) {
+
+	double  radio, area, resultadoArea;
+		Circulo operaciones = new Circulo(); 
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\ningresar el radio r: ");
+		radio = sc.nextFloat();
+
+	
+		System.out.println("\nhalla el area de un ciculo: " + resultadoarea);
+
+	}
+}
